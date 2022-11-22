001e7e1a16f2612603511f894f33bb0b49ff86d6
superiorkid <bayipetani@protonmail.com>
2022-11-22 22:21:08 +0800

    user roles init

--- app/models.py
+++ app/models.py
@@ -1,5 +1,6 @@
 from werkzeug.security import check_password_hash, generate_password_hash
-from flask_login import UserMixin
+from flask_login import UserMixin, AnonymousUserMixin
+from flask import current_app
 
 from . import login_manager, db
 
@@ -10,6 +11,15 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String, unique=True, nullable=False)
     username = db.Column(db.String, unique=True, nullable=False)
     password_hash = db.Column(db.String)
+    role_id = db.Column(db.Integer, db.ForeignKey('role.id'))
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        if self.role is None:
+            if self.email == current_app.config['IS_ADMIN']:
+                self.role = Role.query.filter_by(permissions=0xff).first()
+            if self.role is None:
+                self.role = Role.query.filter_by(default=True).first()
 
     def __repr__(self) -> str:
         return f'<User {self.username}>'
@@ -25,7 +35,56 @@ class User(UserMixin, db.Model):
     def verify_password(self, passwords):
         return check_password_hash(self.password_hash, passwords)
 
+    def can(self, permissions):
+        return self.role is not None and (self.role.permissions & permissions) == permissions
+
+    def is_administrator(self):
+        return self.can(Permission.ADMINISTER)
+
+
+class AnonymousUser(AnonymousUserMixin):
+    def can(self, permissions):
+        return False
+
+    def is_administrator(self):
+        return False
+
+
+login_manager.anonymous_user = AnonymousUser
+
 
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
+
+
+class Role(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+    default = db.Column(db.Boolean, default=False, index=True)
+    permissions = db.Column(db.Integer)
+    users = db.relationship("User", backref="role", lazy="dynamic")
+
+    def __repr__(self):
+        return f"<Role {self.name}>"
+
+    @staticmethod
+    def insert_roles():
+        roles = {
+            'User': (Permission.USERS, True),
+            'Administrator': (0xff, False)
+        }
+
+        for r in roles:
+            role = Role.query.filter_by(name=r).first()
+            if role is None:
+                role = Role(name=r)
+            role.permissions = roles[r][0]
+            role.default = roles[r][1]
+            db.session.add(role)
+        db.session.commit()
+
+
+class Permission:
+    USERS = 0x01
+    ADMINISTER = 0x80
