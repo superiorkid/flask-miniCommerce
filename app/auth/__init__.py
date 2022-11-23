2b447899812be63da44d6df383d26075e2260eb8
superiorkid <bayipetani@protonmail.com>
2022-11-23 18:32:53 +0800

    add app context processor

--- app/auth/__init__.py
+++ app/auth/__init__.py
@@ -1,5 +1,10 @@
 from flask import Blueprint
+from ..models import Permission
 
 auth = Blueprint('auth', __name__)
 
 from . import views
+
+@auth.app_context_processor
+def inject_permissions():
+  return dict(Permission=Permission)
