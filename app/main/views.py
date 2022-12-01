edd6a2dbfc442515222b576b3eb668d671c075ba
superiorkid <bayipetani@protonmail.com>
2022-12-01 20:30:33 +0800

    index views

--- app/main/views.py
+++ app/main/views.py
@@ -2,9 +2,11 @@ from flask_login import login_required
 from flask import render_template
 
 from . import main
+from ..models import Product
 
 
 @main.get('/')
 @login_required
 def index():
-    return render_template('index.html')
+    products = Product.query.all()
+    return render_template('index.html', products=products)
