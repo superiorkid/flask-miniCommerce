5caf4a31b9c815b4dc17c1ec185b782f09f4109d
superiorkid <bayiPetani@protonmail.com>
2023-01-01 23:40:19 +0800

    add badges for the number of shopping baskets

--- app/cart/__init__.py
+++ app/cart/__init__.py
@@ -1,5 +1,15 @@
 from flask import Blueprint
+from flask_login import current_user
 
 cart = Blueprint("cart", __name__)
 
 from . import views
+
+@cart.app_context_processor
+def inject_cart_total():
+    if current_user.is_anonymous:
+        return dict(TotalCart=0)
+
+    return dict(TotalCart=len(current_user.products))
+
+
